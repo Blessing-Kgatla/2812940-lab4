c27c0d5bdeed878db6c8d3b4318d8d427ffe04bf
Blessing-Kgatla <tloukgatla199@gmail.com>
2026-03-02 16:23:35 +0200

    fixed search range

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function searchCountry(countryName) {
         spinner.style.display = 'block';
 
         // Fetch country data
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         const data = await response.json();
         const country = data[0];
 
